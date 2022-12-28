ad4520350f61942c2e227142a443338b2f670b5e
Bren-Dev <brenda_nogueira_pe@hotmail.com>
2022-12-28 16:10:58 -0300

    feat: creating the player's initial structure

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
-
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 console.log(c);
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.width = 30;
+    this.height = 30;
+  }
+  draw() {
+    c.fillStyle = "pink";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const player = new Player();
+player.draw();
