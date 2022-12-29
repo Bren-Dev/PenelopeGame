3e72c12db89ce7adf1beb16d5b527f9344643fdf
Bren-Dev <brenda_nogueira_pe@hotmail.com>
2022-12-28 22:46:56 -0300

    feat: adding gravity

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 console.log(c);
 
+const gravity = 0.5;
+
 class Player {
   constructor() {
     this.position = {
       x: 100,
       y: 100,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
     };
     this.width = 30;
     this.height = 30;
@@ -17,7 +23,20 @@ class Player {
     c.fillStyle = "pink";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+  update() {
+    this.position.y += this.velocity.y;
+    this.draw();
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else this.velocity.y = 0;
+  }
 }
 
 const player = new Player();
-player.draw();
+player.update();
+
+(function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+})();
